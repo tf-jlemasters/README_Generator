434973bc82f0dc1126f3536452bc6099414a1e5e
LeMasters <jacksonlemasters@outlook.com>
2021-06-27 14:51:33 -0600

    first init

---
+++ Develop/index.js
@@ -0,0 +1,85 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [ {
+    type: "input",
+    name: "title",
+    message: "Input your project title"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Provide your project's description"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Provide the instructions for installation"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Provide the project usage"
+},
+{
+    type: "input",
+    name: "licence",
+    message: "Provide the project licence"
+},
+{
+type: "input",
+name: "contributing",
+message: "Please list all of those that contributed"
+},
+{
+type: "input",
+name: "test",
+message: "Please list the project tests"
+},
+{
+    type: "input",
+    name: "username",
+    message: "Input your github user name"
+},
+{
+    type: "input",
+    name: "repo",
+    message: "Input your repo link"
+},
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+        fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+            throw err;
+            };
+    
+            console.log("New README file created with success!");
+        });
+        });
+
+});
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,49 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data, githubInfo) {
+  return `
+# **${data.title}**
+${data.badge}
+## Description 
+${data.description}
+## Table of contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Licence
+${data.licence}
+## Contributors
+${data.contributing}
+## Test
+${data.test}
+## Repository
+- [Project Repo](${data.repo})
+## GitHub
+![Image of me](${githubInfo.githubImage})
+- ${githubInfo.name}
+- [GitHub Profile](${githubInfo.profile})
+- <${githubInfo.email}>
+`;
+}
+
+module.exports = generateMarkdown;
